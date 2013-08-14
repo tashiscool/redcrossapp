0338bfd8929c70089effada99dd69b53dffbd8f3
unknown <tashdid@gmail.com>
2013-08-14 19:25:58 -0400

    adding gitignore

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ app.post('/api/households', households.create);
 //app.put('/api/households/:id', households.update)
 app.del('/api/households/:id', households.del);
 
-app.get('*', function(req, res) {
-  res.sendfile(path.join(clientDir, 'index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendfile(path.join(clientDir, 'index.html'));
+// });
 
 var server = http.createServer(app)
 
